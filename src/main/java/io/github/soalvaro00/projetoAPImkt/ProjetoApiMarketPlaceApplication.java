76e49afd12e352f69a582393027a2cb196d58c5d
soalvaro00 <soalvaro00@gmail.com>
2025-08-26 09:37:39 -0300

    Projeto EcoChain

---
+++ src/main/java/io/github/soalvaro00/projetoAPImkt/ProjetoApiMarketPlaceApplication.java
@@ -0,0 +1,17 @@
+package io.github.soalvaro00.projetoAPImkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiMarketPlaceApplication {
+
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiMarketPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/soalvaro00/projetoAPImkt/ProjetoApiMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.soalvaro00.projetoAPImkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiMarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
